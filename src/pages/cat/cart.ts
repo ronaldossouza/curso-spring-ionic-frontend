01ff2d3042cdd718e5c259aed99d6970e52be7e6
ronaldossouza <ronaldo.ssouzasilva@gmail.com>
2019-04-07 20:34:23 -0300

    Terminando as funcionalidades do carrinho

--- src/pages/cat/cart.ts
+++ src/pages/cat/cart.ts
@@ -4,6 +4,7 @@ import { CartItem } from '../../models/cart-item';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CartService } from '../../services/domain/cart.service';
+import { ProdutoDTO } from '../../models/produto.dto';
 
 @IonicPage()
 @Component({
@@ -11,22 +12,18 @@ import { CartService } from '../../services/domain/cart.service';
   templateUrl: 'cart.html',
 })
 export class CartPage {
-
   items: CartItem[];
-
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public cartService: CartService,
     public produtoService: ProdutoService) {
   }
-
   ionViewDidLoad() {
     let cart = this.cartService.getCart();
     this.items = cart.items;
     this.loadImageUrls();
   }
-
   loadImageUrls() {
     for (var i=0; i<this.items.length; i++) {
       let item = this.items[i];
@@ -36,5 +33,25 @@ export class CartPage {
         },
         error => {});
     }
+  }
+
+  removeItem(produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+
+  decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+
+  total() : number {
+    return this.cartService.total();
+  }
+
+  goOn() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,11 +5,19 @@ import { API_CONFIG } from "../config/api.config";
 import { LocalUser } from "../models/local_user";
 import { StorageService } from "./storage.service";
 import { JwtHelper } from 'angular2-jwt';
+import { CartService } from "./domain/cart.service";
+
 @Injectable()
 export class AuthService {
+
     jwtHelper: JwtHelper = new JwtHelper();
-    constructor(public http: HttpClient, public storage: StorageService) {
+
+    constructor(
+        public http: HttpClient,
+        public storage: StorageService,
+        public cartService: CartService) {
     }
+
     authenticate(creds : CredenciaisDTO) {
         return this.http.post(
             `${API_CONFIG.baseUrl}/login`,
@@ -19,7 +27,6 @@ export class AuthService {
                 responseType: 'text'
             });
     }
-
     refreshToken() {
         return this.http.post(
             `${API_CONFIG.baseUrl}/auth/refresh_token`,
@@ -29,7 +36,6 @@ export class AuthService {
                 responseType: 'text'
             });
     }
-
     successfulLogin(authorizationValue : string) {
         let tok = authorizationValue.substring(7);
         let user : LocalUser = {
@@ -37,7 +43,9 @@ export class AuthService {
             email: this.jwtHelper.decodeToken(tok).sub
         };
         this.storage.setLocalUser(user);
+        this.cartService.createOrClearCart();
     }
+
     logout() {
         this.storage.setLocalUser(null);
     }
