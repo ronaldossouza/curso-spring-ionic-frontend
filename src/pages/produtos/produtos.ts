36cabbddab798c075c56acfd58ee7979c755ed10
ronaldossouza <ronaldo.ssouzasilva@gmail.com>
2019-04-07 18:55:36 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -3,31 +3,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { API_CONFIG } from '../../config/api.config';
 import { ProdutoService } from '../../services/domain/produto.service';
-
 @IonicPage()
 @Component({
   selector: 'page-produtos',
   templateUrl: 'produtos.html',
 })
 export class ProdutosPage {
-
   items : ProdutoDTO[];
-
-
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public produtoService: ProdutoService) {
   }
-
   ionViewDidLoad() {
-
     let categoria_id = this.navParams.get('categoria_id');
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrls();
       },
-
       error => {});
+  }
+
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
+    }
   }
 }
