8e9760160ca94f09cf8d427c7b3b290f19f4a738
ronaldossouza <ronaldo.ssouzasilva@gmail.com>
2019-04-07 21:09:24 -0300

    Buscando endereço do banco de dados

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,35 +4,37 @@ import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
-
 @IonicPage()
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-
   cliente: ClienteDTO;
-
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService) {
   }
-
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
-          this.cliente = response;
+          this.cliente = response as ClienteDTO;
           this.getImageIfExists();
         },
-        error => {});
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
-
   getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
     .subscribe(response => {
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -8,14 +8,15 @@ import { StorageService } from "../storage.service";
 export class ClienteService {
     constructor(public http: HttpClient, public storage: StorageService) {
     }
-    findByEmail(email: string) : Observable<ClienteDTO> {
-        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+
+    findByEmail(email: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
+
     getImageFromBucket(id : string) : Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
         return this.http.get(url, {responseType : 'blob'});
     }
-
     insert(obj : ClienteDTO) {
         return this.http.post(
             `${API_CONFIG.baseUrl}/clientes`,
