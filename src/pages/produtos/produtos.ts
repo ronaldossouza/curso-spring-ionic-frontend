3a7a1feb81b9431901b63ce005aa535cd1b4d3f7
ronaldossouza <ronaldo.ssouzasilva@gmail.com>
2019-04-23 06:36:50 -0300

    Infinite scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -75,7 +75,7 @@ export class ProdutosPage {
     this.loadData();
     setTimeout(() => {
       refresher.complete();
-    }, 2000);
+    }, 1000);
   }
 
   doInfinite(infiniteScroll) {
@@ -83,6 +83,6 @@ export class ProdutosPage {
     this.loadData();
     setTimeout(() => {
       infiniteScroll.complete();
-    }, 2000);
+    }, 1000);
   }
 }
